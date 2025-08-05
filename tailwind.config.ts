694199588662e25545dcb6b5c3e2fb56d6413c41
sailesh-010 <saileshshahi2205bit@balmikilincoln.edu.np>
2025-08-05 09:42:27 +0545

    hello

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.ts
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+    "./section/**/*.{html,js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
